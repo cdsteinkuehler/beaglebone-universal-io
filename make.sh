05a0f02bec7543825cb33d9f5ab6b61dbb44d20f
Charles Steinkuehler <charles@steinkuehler.net>
2014-03-23 16:22:25 +0000

    Add cape version that does not use audio pins
    Signed-off-by: Charles Steinkuehler <charles@steinkuehler.net>

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 dtc -@ -I dts -O dtb -o /lib/firmware/cape-universal-00A0.dtbo cape-universal-00A0.dts
+dtc -@ -I dts -O dtb -o /lib/firmware/cape-universaln-00A0.dtbo cape-universaln-00A0.dts
 dtc -@ -I dts -O dtb -o /lib/firmware/cape-univ-emmc-00A0.dtbo cape-univ-emmc-00A0.dts
